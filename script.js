4df44e184606aff9e233e0b58dbc2a3a6a6a2869
Harsh-Awasthi <115348870+Harsh-Awasthi@users.noreply.github.com>
2024-06-17 10:13:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function firstChar(text) {
-  // your code here
-}
-
-// Do not change the code below
-
-const text = prompt("Enter text:");
-alert(firstChar(text));
+    return text.charAt(0);
+  }
+  
+  // Do not change the code below
+  
+  const text = prompt("Enter text:");
+  alert(firstChar(text));
